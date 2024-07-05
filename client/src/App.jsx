6ba6330ba4466bfb200f8f2cafc1e46e0bc87a7e
นภสร ทองโสภา <napasorn.tsp@gmail.com>
2024-07-06 01:04:26 +0700

    submit:React Context Api Assignment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { createContext } from "react";
 
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
+
+export const UserDataContext = createContext()
 
 function App() {
   const userData = {
@@ -13,12 +16,14 @@ function App() {
 
   return (
     <div className="App">
+      <UserDataContext.Provider value={userData}>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/product/view/:id" element={<ViewProductPage />} />
         </Routes>
       </Router>
+      </UserDataContext.Provider>
     </div>
   );
 }
